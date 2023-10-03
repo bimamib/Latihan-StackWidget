e7be6f8f363adb48563a85c13f450dd7ad5266e3
bimamib <bimaprast1601@gmail.com>
2023-10-03 21:23:00 +0700

    mengimplementasi kelas

---
+++ app/src/main/java/com/bima/mystackwidget/StackRemoteViewsFactory.kt
@@ -0,0 +1,42 @@
+package com.bima.mystackwidget
+
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+
+internal class StackRemoteViewsFactory : RemoteViewsService.RemoteViewsFactory{
+    override fun onCreate() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDataSetChanged() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroy() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getViewAt(p0: Int): RemoteViews {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLoadingView(): RemoteViews {
+        TODO("Not yet implemented")
+    }
+
+    override fun getViewTypeCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasStableIds(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
